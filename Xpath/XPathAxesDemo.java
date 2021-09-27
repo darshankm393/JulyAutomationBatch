e20e9a91bfb6b1d60ab5682e3e5369745d68bd29
darshankm393 <90144398+darshankm393@users.noreply.github.com>
2021-09-27 17:30:04 +0530

    Update XPathAxesDemo.java

--- Xpath/XPathAxesDemo.java
+++ Xpath/XPathAxesDemo.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class XPathAxesDemo {
+public class   XPathAxesDemo {
 
 	public static WebDriver oBrowser=null;
 	public static void main(String[] args) {
